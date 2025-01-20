0076dd7d3225edbd2337a2efcda33e6d362d76f9
Teertharaj Turkar <teertharajturkar@gmail.com>
2025-01-20 21:18:21 +0530

    day 33 react icons website for the project

---
+++ my notes/33_Icons_website_reactJs.md
@@ -0,0 +1,19 @@
+****33: Best Website to Find Icons for Your React App: Complete Installation and Usage Guide*****
+------------------------------------------------
+Welcome, In this video, learn how to find, install, and use high-quality icons in your React.js projects. We'll reveal the best website for sourcing icons and demonstrate step-by-step how to integrate and style them in your React applications. Perfect for enhancing your app's UI with professional icons. Real-life example: Add a stylish to-do list icon to your project! 
+
+website for icons :-https://react-icons.github.io/react-icons/
+
+npm package link:-https://www.npmjs.com/package/react-icons
+
+step 01:- npm i react-icons
+
+step 2:- import this link in project of jsx file
+import { IoIosSwitch } from "react-icons/io";
+
+step 3:- use as components there required
+<IoIosSwitch />
+
+
+step 4 changing css praperty
+style={{ color: "red", textAlign: "center" }}
--- src/projects/ToggleSwitch/ToggleSwitch.jsx
+++ src/projects/ToggleSwitch/ToggleSwitch.jsx
@@ -1,5 +1,36 @@
+// import { useState } from "react";
+// import "./ToggleSwitch.css";
+
+// export const ToggleSwitch = () => {
+//   const [isOn, setIsOn] = useState(false);
+
+//   const handleToggleSwitch = () => {
+//     setIsOn(!isOn);
+//   };
+
+//   const checkIsOn = isOn ? "on" : "off";
+//   const toggleBGColor = { backgroundColor: isOn ? "#4caf50" : "" };
+
+//   return (
+//     <>
+//       <div
+//         className="toggle-switch"
+//         style={toggleBGColor}
+//         onClick={handleToggleSwitch}
+//       >
+//         <div className={`switch ${checkIsOn}`}>
+//           <span className="switch-state">{checkIsOn}</span>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+
 import { useState } from "react";
 import "./ToggleSwitch.css";
+import { IoIosSwitch } from "react-icons/io";
+import { PiUserSwitchFill } from "react-icons/pi";
 
 export const ToggleSwitch = () => {
   const [isOn, setIsOn] = useState(false);
@@ -13,6 +44,11 @@ export const ToggleSwitch = () => {
 
   return (
     <>
+      <h1 style={{ color: "#000", textAlign: "center" }}>
+        Toggle Switch{" "}
+        <IoIosSwitch style={{ color: "red", textAlign: "center" }} />
+        <PiUserSwitchFill />
+      </h1>
       <div
         className="toggle-switch"
         style={toggleBGColor}
@@ -25,4 +61,5 @@ export const ToggleSwitch = () => {
     </>
   );
 };
+
 
