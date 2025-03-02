981293a0c23f1440a0db474e369c5a71f6ebd352
Teertharaj Turkar <teertharajturkar@gmail.com>
2025-03-02 18:00:23 +0530

    day 88:Final UI Upgrades: Perfecting Your React Movie Website RR13

---
+++ my notes/88_Final UI Upgrades Perfecting Your React Movie Website_RR13.md
@@ -0,0 +1,5 @@
+***88:Final UI Upgrades: Perfecting Your React Movie Website RR13******
+In this video, we put the finishing touches on our React Movie Website, enhancing the UI of the home, movie, and contact pages. With all the functionality in place, this video focuses on upgrading the design to ensure a polished and professional look. Whether you're building your first project or refining an existing one, these final steps will help you create a website that truly stands out. 💸 
+
+
+focus on design and responsive website
--- react_router/src/components/layout/Footer.jsx
+++ react_router/src/components/layout/Footer.jsx
@@ -1,47 +1,43 @@
-export const Footer=()=>{
-    return (<>
-    
-
-    <footer className="section-footer">
-      <div className="footer-container container">
-        <div className="content_1">
-          <img src="/jsecom_youtube/public/images/logo.png" alt="logo" />
-          <p>
-            Welcome to Thapa EcomStore, your ultimate destination for
-            cutting-edge gadgets!
-          </p>
-          <img src="https://i.postimg.cc/Nj9dgJ98/cards.png" alt="cards" />
-        </div>
-        <div className="content_2">
-          <h4>SHOPPING</h4>
-          <a href="#">Computer Store</a>
-          <a href="#">Laptop Store</a>
-          <a href="#">Accessories</a>
-          <a href="#">Sales & Discount</a>
-        </div>
-        <div className="content_3">
-          <h4>Experience</h4>
-          <a href="./contact.html">Contact Us</a>
-          <a href="" target="_blank">Payment Method</a>
-          <a href="" target="_blank">Delivery</a>
-          <a href="" target="_blank">Return and Exchange</a>
-        </div>
-        <div className="content_4">
-          <h4>NEWSLETTER</h4>
-          <p>Be the first to know about new<br />arrivals, sales & promos!</p>
-          <div className="f-mail">
-            <input type="email" placeholder="Your Email" />
-            <i className="bx bx-envelope"></i>
+export const Footer = () => {
+  return (
+    <>
+      <footer className="section-footer">
+        <div className="footer-container container">
+          <div className="content_1">
+            <p>MovieMix</p>
+            <p>
+              Welcome to Moviemix, your ultimate destination for funtastic
+              movies paltform !
+            </p>
+            <img src="https://i.postimg.cc/Nj9dgJ98/cards.png" alt="cards" />
           </div>
-          <hr />
+          <div className="content_2">
+            <h4>Check out movies here</h4>
+            <a href="#">Computer Store</a>
+            <a href="#">Laptop Store</a>
+            <a href="#">Accessories</a>
+            <a href="#">Sales & Discount</a>
+          </div>
+          <div className="content_3">
+            <h4>Experience</h4>
+            <a href="./contact.html">Contact Us</a>
+            <a href="" target="_blank">
+              Payment Method
+            </a>
+            <a href="" target="_blank">
+              Fun and Romantic
+            </a>
+            <a href="" target="_blank">
+              high quality download
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="f-design">
-        <div className="f-design-txt">
-          <p>Design and Code by Teertharaj Turkar</p>
+        <div className="f-design">
+          <div className="f-design-txt">
+            <p style={{"color":"green"}}>Design and Code by Teertharaj Turkar</p>
+          </div>
         </div>
-      </div>
-    </footer>
-
-    </>);
-}
+      </footer>
+    </>
+  );
+};
